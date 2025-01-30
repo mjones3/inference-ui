0c9a4dd0dd9e36e3a59c07dfd96a0d6d546cd957
mjones3@github.com <mjones3@gmail.com>
2025-01-29 19:16:46 -0500

    changed HUGGING_FACE_API_URL

--- amplify/functions/sentimentAnalysis/handler.ts
+++ amplify/functions/sentimentAnalysis/handler.ts
@@ -6,8 +6,7 @@ import { DynamoDB, SecretsManager } from "aws-sdk";
 const dynamoDb = new DynamoDB.DocumentClient();
 const DYNAMODB_TABLE_NAME = "Tweets";
 const HUGGING_FACE_API_URL =
-  "https://lsrt5bkedfuuxkrd.us-east-1.aws.endpoints.huggingface.cloud";
-
+  "https://rxrvkd3gxtl2id45.us-east-1.aws.endpoints.huggingface.cloud";
 const secretsManager = new SecretsManager();
 
 // Function to retrieve secrets from AWS Secrets Manager
